2f20268e9c1f7fd4934b9ea54a44e9a841659172
felipe-bigoto <felipebigoto@hotmail.com>
2026-03-13 16:11:19 -0300

    Update send.js

--- js/send.js
+++ js/send.js
@@ -1,6 +1,15 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbyYf5pmLjyxdt_EMgwTF2wdWtQbqtY5peix6RPK-WMsKP2pAtkKnxs66zCq-MvoRvXkEw/exec"
 
+let enviando = false
+
 async function finalizar(){
+
+if(enviando) return
+enviando = true
+
+let botao = document.getElementById("btnFinalizar")
+botao.disabled = true
+botao.innerText = "Enviando..."
 
 let dados = JSON.parse(localStorage.getItem("dados")) || {}
 
@@ -24,6 +33,10 @@ window.location.href="obrigado.html"
 }catch(e){
 
 alert("Erro ao enviar respostas")
+
+botao.disabled = false
+botao.innerText = "Finalizar pesquisa"
+enviando = false
 
 }
 
